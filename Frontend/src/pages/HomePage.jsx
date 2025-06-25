a722107cc1f0072671eebb3fc6db5866eaf3b8c4
Sourav <souravmohanta4428@gmail.com>
2025-06-26 02:15:27 +0530

    updated homepage styling and layout

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -20,14 +20,15 @@ function HomePage() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col items-center mt-14 px-4">
-      <div className="absolute top-16 left-0 w-1/3 h-1/3 bg-primary opacity-30 blur-3xl rounded-md bottom-9"></div>
-      <h1 className="text-4xl font-extrabold z-10 text-center">
+    <div className="min-h-screen flex flex-col items mt-14 px-4 bg-black">
+      <div className="absolute top-16 left-0 w-1/3 h-1/3 opacity-30 blur-3xl rounded-md bottom-9"></div>
+      <h1 className="text-4xl font-extrabold z-10 ">
         Welcome to <span className="text-blue-600">AlgoPundit</span>
       </h1>
 
-      <p className="mt-4 text-center text-lg font-semibold text-gray-500 dark:text-gray-400 z-10">
-       You’ve just taken the first step toward mastering coding interviews. Dive into handpicked problems, track your progress, and build the confidence to crack even the toughest technical rounds.
+      <p className="mt-4  text-lg font-semibold text-gray-500 dark:text-gray-400 z-10">
+       You’ve just taken the first step toward mastering coding interviews. Dive into handpicked problems, track <br/>
+       your progress, and build the confidence to crack even the toughest technical rounds.
       </p>
 
        {
